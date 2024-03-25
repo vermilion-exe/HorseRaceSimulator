5c17e8ba67e6ba0968eda07f21ffc068dd2a6d5d
vermilion-exe <83659123+vermilion-exe@users.noreply.github.com>
2024-03-25 15:31:54 +0000

    Initial commit

---
+++ Horse.java
@@ -0,0 +1,82 @@
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author Farhad Garaisa
+ * @version 1.0
+ */
+public class Horse
+{
+    //Fields of class Horse
+    private String name;
+    private Character symbol;
+    private double confidence;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        name = horseName;
+        symbol = horseSymbol;
+        confidence = horseConfidence;
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return confidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public char getSymbol()
+    {
+        return symbol;
+    }
+    
+    public void goBackToStart()
+    {
+        distanceTravelled = 0;
+    }
+    
+    public boolean hasFallen()
+    {
+        return hasFallen;
+    }
+
+    public void moveForward()
+    {
+        distanceTravelled++;   
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        confidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        symbol = newSymbol;
+    }
+    
+}
