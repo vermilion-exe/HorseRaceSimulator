4c7dbb93a66b9249b76a8b4d19eaefd94d96b1de
vermilion-exe <83659123+vermilion-exe@users.noreply.github.com>
2024-03-29 14:29:24 +0000

    Changes to HorseAddition and RaceControl windows

--- Part2/HorseAdditionWindow.java
+++ Part2/HorseAdditionWindow.java
@@ -3,17 +3,20 @@ package Part2;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
 import java.awt.event.*;
 
+import Part1.Breed;
 import Part1.Horse;
 import Part1.Race;
 
 public class HorseAdditionWindow {
 
     private JFrame window;
-    private int currentHeight = 50;
+    private int currentHeight = 80;
 
     public HorseAdditionWindow(Race race) {
         window = new JFrame("Add Horse");
@@ -21,6 +24,7 @@ public class HorseAdditionWindow {
         window.setSize(800, 600);
         window.setLocationRelativeTo(null);
         window.setLayout(null);
+        window.getContentPane().setBackground(Color.decode("#74add6"));
 
 
         // JPanel panel = new JPanel();
@@ -28,29 +32,37 @@ public class HorseAdditionWindow {
         // panel.setPreferredSize(new Dimension(200, 30));
 
         JLabel horseNameLabel = new JLabel("Horse Name:");
-        horseNameLabel.setBounds(50, 100, 200, 30);
+        horseNameLabel.setBounds(50, 30, 200, 30);
         JTextField horseNameField = new JTextField("Name");
-        horseNameField.setBounds(50, 150, 200, 30);
+        horseNameField.setBounds(50, 80, 200, 30);
 
-        JLabel horseSymbolLabel = new JLabel("Horse Symbol:");
-        horseSymbolLabel.setBounds(50, 200, 200, 30);
-        JTextField horseSymbolField = new JTextField("Symbol");
-        horseSymbolField.setBounds(50, 250, 200, 30);
+        // JLabel horseSymbolLabel = new JLabel("Horse Symbol:");
+        // horseSymbolLabel.setBounds(50, 130, 200, 30);
+        // JTextField horseSymbolField = new JTextField("Symbol");
+        // horseSymbolField.setBounds(50, 180, 200, 30);
 
         JLabel horseConfidenceLabel = new JLabel("Horse Confidence:");
-        horseConfidenceLabel.setBounds(50, 300, 200, 30);
+        horseConfidenceLabel.setBounds(50, 130, 200, 30);
         JTextField horseConfidenceField = new JTextField("Confidence");
-        horseConfidenceField.setBounds(50, 350, 200, 30);
+        horseConfidenceField.setBounds(50, 180, 200, 30);
+
+        JLabel horseBreedLabel = new JLabel("Horse Breed:");
+        horseBreedLabel.setBounds(50, 230, 200, 30);
+        JComboBox horseBreedField = new JComboBox(new String[]{"Haflinger", "Andalusian", "Eriskay", "Caspian", "Arabian", "Aegidienberger", "Connemara", "Morgan"});
+        horseBreedField.setBounds(50, 280, 200, 30);
 
         JButton addButton = new JButton("Add Horse");
-        addButton.setBounds(50, 400, 200, 30);
+        addButton.setBounds(50, 330, 200, 30);
 
         JButton continueButton = new JButton("Continue");
-        continueButton.setBounds(50, 450, 200, 30);
+        continueButton.setBounds(50, 380, 200, 30);
+
+        JLabel horseLabel = new JLabel("Horses:");
+        horseLabel.setBounds(500, 30, 200, 30);
 
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (horseNameField.getText().equals("") || horseSymbolField.getText().equals("") || horseConfidenceField.getText().equals("")) {
+                if (horseNameField.getText().equals("") || horseConfidenceField.getText().equals("")) {
                     JOptionPane.showMessageDialog(window, "Please fill in all the fields", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
@@ -65,10 +77,10 @@ public class HorseAdditionWindow {
                     JOptionPane.showMessageDialog(window, "Confidence should be a number between 0 and 1!", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
-                Horse horse = new Horse(horseSymbolField.getText().charAt(0), horseNameField.getText(), confidence);
+                Horse horse = new Horse(horseNameField.getText(), confidence, Breed.valueOf(horseBreedField.getSelectedItem().toString()));
                 race.addHorse(horse);
                 JLabel addedLabel = new JLabel(horse.getName()+" added! Current confidence: "+horse.getConfidence());
-                addedLabel.setBounds(600, currentHeight, 200, 30);
+                addedLabel.setBounds(500, currentHeight, 300, 30);
                 currentHeight += 50;
                 window.add(addedLabel); // Add the label to the panel
                 window.revalidate(); // Revalidate the window to update the UI
@@ -76,14 +88,29 @@ public class HorseAdditionWindow {
             }
         });
 
+        continueButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(race.getHorses().size()<2){
+                    JOptionPane.showMessageDialog(window, "You need at least 2 horses to start the race!", "Warning", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                window.setVisible(false);
+                RaceControlWindow raceWindow = new RaceControlWindow(race);
+                raceWindow.setVisible(true);
+            }
+        });
+
         window.add(horseNameLabel);
         window.add(horseNameField);
-        window.add(horseSymbolLabel);
-        window.add(horseSymbolField);
+        // window.add(horseSymbolLabel);
+        // window.add(horseSymbolField);
         window.add(horseConfidenceLabel);
         window.add(horseConfidenceField);
+        window.add(horseBreedLabel);
+        window.add(horseBreedField);
         window.add(addButton);
         window.add(continueButton);
+        window.add(horseLabel);
 
     }
     
---
+++ Part2/RaceControlWindow.java
@@ -0,0 +1,76 @@
+package Part2;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import Part1.Race;
+
+public class RaceControlWindow {
+    private JFrame window;
+
+    public RaceControlWindow(Race race) {
+        window = new JFrame("Add Horse");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setLayout(null);
+        window.getContentPane().setBackground(Color.decode("#74add6"));
+        
+        JLabel raceLengthLabel = new JLabel("Race track length: "+race.getRaceLength());
+        raceLengthLabel.setBounds(30, 30, 200, 30);
+        JSlider raceLengthSlider = new JSlider(5, 30, 5);
+        raceLengthSlider.setBounds(300, 30, 200, 50);
+        raceLengthSlider.setPaintTrack(true);
+        raceLengthSlider.setPaintTicks(true);
+        raceLengthSlider.setPaintLabels(true);
+        raceLengthSlider.setMajorTickSpacing(50);
+        raceLengthSlider.setMinorTickSpacing(5);
+        JButton startRaceButton = new JButton("Start Race!");
+        startRaceButton.setBounds(300, 500, 200, 30);
+
+        int horseY = 150;
+        int horseX = 30;
+        for(int i = 0; i<race.getHorses().size(); i++) {
+            if(i%5 == 0 && i != 0) {
+                horseY += 150;
+                horseX = 30;
+            }
+            Image img = null;
+            try {
+                String imageLink = race.getHorses().get(i).getImageLinks()[0];
+                img = ImageIO.read(getClass().getResource(imageLink));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            JButton horse = new JButton(new ImageIcon(img));
+            JLabel horseNameLabel = new JLabel(race.getHorses().get(i).getName());
+            horse.setBounds(horseX, horseY, 100, 100);
+            horseNameLabel.setBounds(horseX, horseY+100, 100, 30);
+            horseX += 155;
+            window.add(horse);
+            window.add(horseNameLabel);
+        }
+
+        raceLengthSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                race.setRaceLength(raceLengthSlider.getValue());
+                raceLengthLabel.setText("Race track length: "+race.getRaceLength());
+            }
+        });
+
+        window.add(raceLengthLabel);
+        window.add(raceLengthSlider);
+        window.add(startRaceButton);
+    }
+
+    public void setVisible(boolean visible) {
+        window.setVisible(visible);
+    }
+    
+}
