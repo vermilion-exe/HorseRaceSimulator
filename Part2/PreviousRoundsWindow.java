bc0ea7cddd07a31cbb927401e2eb4daf9bfead0f
vermilion-exe <83659123+vermilion-exe@users.noreply.github.com>
2024-04-24 00:58:30 +0100

    PreviousRound window modified
    The layout of the panel changed to allow for the scroll function and the buttons now have a background image. Back button was added.

--- Part2/PreviousRoundsWindow.java
+++ Part2/PreviousRoundsWindow.java
@@ -3,6 +3,9 @@ package Part2;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 
 import javax.imageio.ImageIO;
@@ -17,30 +20,48 @@ public class PreviousRoundsWindow extends Window {
         
         JPanel roundPanel = new JPanel();
         roundPanel.setBackground(Color.decode("#74add6"));
-        roundPanel.setLayout(new BoxLayout(roundPanel, BoxLayout.Y_AXIS));
+        roundPanel.setLayout(new GridBagLayout());
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
+        for(Round round : race.getRounds()) {  
+            ImageIcon icon = null;     
+            try{
+                Image img = ImageIO.read(getClass().getResource("resources/RoundBackground.jpg"));
+                icon = new ImageIcon(img.getScaledInstance(730, 100, Image.SCALE_SMOOTH));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
 
-        for(Round round : race.getRounds()) {
-            JButton roundInfo = new JButton("Round #"+round.getRoundNumber());
-            roundInfo.setMaximumSize(new Dimension(800, 100));
-            
-            // try{
-            //     Image img = ImageIO.read(getClass().getResource("resources/race-sprites/" + round.getLaneType() + "_0.png"));
-            //     roundInfo.setIcon(new ImageIcon(img));
-            // }
-            // catch (Exception e) {
-            //     e.printStackTrace();
-            // }
-            
+            JButton roundInfo = new JButton("Round #"+round.getRoundNumber(), icon);
+            roundInfo.setPreferredSize(new Dimension(700, 100));
+            JLabel spacing = new JLabel(" ");
+
+            roundInfo.setHorizontalTextPosition(SwingConstants.CENTER);
+            roundInfo.setFont(new Font("Arial", Font.PLAIN, 24));
+        
             roundInfo.addActionListener(e -> {
                 window.dispose();
-                RoundWindow roundWindow = new RoundWindow(round);
+                RoundWindow roundWindow = new RoundWindow(race, round);
                 roundWindow.setVisible(true);
             });
-
-            roundPanel.add(roundInfo);
+        
+            roundPanel.add(roundInfo, gbc);
+            roundPanel.add(spacing, gbc);
         }
         
         JScrollPane scrollPane = new JScrollPane(roundPanel);
         window.add(scrollPane);
+
+        JButton backButton = new JButton("Back");
+        backButton.addActionListener(e -> {
+            window.dispose();
+            RaceControlWindow raceControlWindow = new RaceControlWindow(race);
+            raceControlWindow.setVisible(true);
+        });
+        window.add(backButton, BorderLayout.SOUTH);
     }
 }
